d010c0ba68b2dadfeaabdfafd1e2cc4e2dae8bbb
Valiant John <84314269+ValiantJohnCh@users.noreply.github.com>
2022-08-07 05:01:27 +0530

    updated scripts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import core from '@actions/core'
 import fs from 'fs';
 import { EOL } from 'os';
 
 function run(): void {
+  const incrementBuildNumber = core.getBooleanInput('incrementBuildNumber');
+  
   const filePath = 'ProjectSettings/ProjectSettings.asset';
   const settingsFile = fs.readFileSync(filePath, 'utf8');
 
@@ -19,7 +22,9 @@ function run(): void {
 
   let modifiedFile = settingsFile;
   let buildNumber = parseInt(buildNumberMatch[1]);
-  buildNumber++;
+
+  if(incrementBuildNumber)
+    buildNumber++;
 
   modifiedFile = modifiedFile.replace(buildNumberMatch[0], `AndroidBundleVersionCode: ${buildNumber}`);
   modifiedFile = modifiedFile.replace(regexTwoMatch[0], `buildNumber:${EOL}    Standalone: ${buildNumber}${EOL}    iPhone: ${buildNumber}${EOL}    tvOS: ${buildNumber}`)
